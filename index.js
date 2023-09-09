d05d5d42d415fe565025468ecf1ab6ca8db29a0f
abdelmounim-dev <77006851+abdelmounim-dev@users.noreply.github.com>
2023-09-09 18:10:21 +0000

    everything except Each element in the array returned from GET /api/users is an object literal containing a user's username and _id. works

--- index.js
+++ index.js
@@ -4,11 +4,93 @@ const cors = require('cors')
 require('dotenv').config()
 
 app.use(cors())
+app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const users = []
+const data = []
+
+app.post('/api/users', (req, res) => {
+  const username = req.body.username
+  if(!username) {
+    res.send({error});
+    return;
+  }
+
+  const _id = users.length;
+  users.push({username, log:[]});
+
+  res.send({username, _id})
+
+})
+
+app.get('/api/users', (req, res) => {
+  let usersObject = [];
+  users.forEach((user, i) => usersObject.push({ _id: i + 1, username: user.username}))
+  // console.log(usersObject)
+  res.send(usersObject)
+})
+
+app.post('/api/users/:id/exercises', (req, res) => {
+  const _id = Number(req.params.id);
+  const date = req.body.date? new Date(req.body.date): new Date()
+
+
+
+  let dateString = date.toDateString();
+  if(dateString === 'Invalid Date') console.log(req.body.date)
+
+  let workout = {
+    description:  req.body.description,
+    duration: Number(req.body.duration),
+    date: dateString
+  }
+
+  users[_id].log.push(workout)
+  workout = {username: users[_id].username, _id, ...workout }
+  res.send(workout)
+})
+
+app.get('/api/users/:id/logs', (req, res) => {
+  const {limit, from , to} = req.query
+
+  const _id = req.params.id;
+  if (_id > users.length - 1) {
+    res.send({error})
+    return;
+  }
+  const {log, ...rest} = users[_id];
+  let filteredLog =  log
+
+  if(from) {
+    fromDate =Date.parse(from)
+    const logs = filteredLog.filter(log => {
+      let date = Date.parse(log.date)
+      return date > fromDate
+    })
+    filteredLog = logs
+  }
+  if (to) {
+    toDate = Date.parse(to)
+    const logs = filteredLog.filter(log => {
+      let date = Date.parse(log.date)
+      return date < toDate
+    })
+
+    filteredLog = logs
+  }
+
+  if (limit) filteredLog = filteredLog.slice(0, limit)
+
+  const logs = {...rest, log:  filteredLog, count: filteredLog.length}
+  if(from || to || limit)
+  console.log(">>>\n\n",users[_id], logs, {from, to, limit}, "<<<\n\n")
+
+  res.send(logs)
+})
 
 
 
